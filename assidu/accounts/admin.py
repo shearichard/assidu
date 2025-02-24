f14affd75f8758ff30337cb2f0fc24e57877fdc6
Richard Shea <rshea@thecubagroup.com>
2025-02-25 12:51:01 +1300

    Make uses visible in Admin; Also change the name displayed on Admin logon/index pages

--- assidu/accounts/admin.py
+++ assidu/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+admin.site.site_title = 'Assidu'
+admin.site.site_header = 'Assidu Admin'
+admin.site.index_title = 'Assidu Index'
+
+
 # Register your models here.
+from django.contrib import admin
+from accounts.models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
