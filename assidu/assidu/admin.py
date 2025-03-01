791360f49c20b47554279ec604912c32bab9fc92
Richard Shea <rshea@thecubagroup.com>
2025-03-01 23:03:48 +1300

    Put in place a country/city model to allow easy testing of pytest

--- assidu/assidu/admin.py
+++ assidu/assidu/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 
 from .models import Task, Accomplishment 
+from .models import Country, City 
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('description', 'user', 'startdate', 'enddate', 'interval_days')
 
+
 @admin.register(Accomplishment)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('task__description', 'task__user', 'date', 'notes')
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('country_iso_code', 'population', 'area_sq_km')
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('country__country_iso_code', 'mayor_name', 'date_of_last_mayoral_election', 'area_sq_km', 'elevation_metres')
--- assidu/assidu/models.py
+++ assidu/assidu/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from datetime import date
 from django.utils import timezone
 from accounts.models import User
+from .model_static_data import COUNTRY_ISO_CODES
 
 
 class Task(models.Model):
+    '''
+    Represents something which the user wishes to do, typically
+    every day, but possibly less frequently, say every seven days
+    '''
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     description = models.CharField(max_length=255)
     startdate = models.DateField(default=date.today)
@@ -13,6 +18,57 @@ class Task(models.Model):
 
 
 class Accomplishment(models.Model):
+    '''
+    Represents a time when the user accomplished a given task
+    '''
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     date = models.DateField()
     notes = models.CharField(max_length=255, blank=True)
+
+
+
+class Country(models.Model):
+    '''
+    Represents a Country.
+
+    NB: This model will only exist for a short time while
+    some aspects of pytest are sorted out
+    '''
+
+    COUNTRIES = COUNTRY_ISO_CODES
+
+    country_iso_code = models.CharField(max_length=3, choices = COUNTRIES)
+    population = models.IntegerField()
+    area_sq_km = models.IntegerField()
+
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
+    def __str__(self):
+        return self.country_iso_code
+
+
+class City(models.Model):
+    '''
+    Represents a City.
+
+    NB: This model will only exist for a short time while
+    some aspects of pytest are sorted out
+    '''
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    city_name = models.CharField(max_length=255)
+    mayor_name = models.CharField(max_length=255)
+    date_of_last_mayoral_election = models.DateField()
+    population = models.IntegerField()
+    area_sq_km = models.IntegerField()
+    elevation_metres = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = "Cities"
+
+
+    def __str__(self):
+        return f"{self.city_name} ({self.country.country_iso_code})"
+
