d955fdc8efb1587f5a3e877df50a0007d9f65d6a
Richard Shea <rshea@thecubagroup.com>
2025-03-05 16:59:00 +1300

    Fleshed out test of City Form now working

--- assidu/assidu/tests/test_country.py
+++ assidu/assidu/tests/test_country.py
@@ -46,50 +46,49 @@ class CountryTest(TestCase):
     # Test views
     def test_valid_form(self):
         c = Country.objects.create(country_iso_code="USA", population=200, area_sq_km=201)
-        print("Test valid form A0")
-        print(c.country_iso_code)
-        print(c.population)
-        print(c.area_sq_km)
-        print("Test valid form A1")
-        data = { "country_iso_code": "Angola" , "population": c.population , "area_sq_km": c.area_sq_km }
-        data = { "id_country_iso_code": c.country_iso_code, "id_population": c.population , "id_area_sq_km": c.area_sq_km }
         data = { "country_iso_code": c.country_iso_code, "population": c.population , "area_sq_km": c.area_sq_km }
-        data = { "id_country_iso_code": "Angola" , "id_population": c.population , "id_area_sq_km": c.area_sq_km }
-        data = { "country_iso_code": "Angola" , "population": c.population , "area_sq_km": c.area_sq_km }
-        data = { "country_iso_code": c.country_iso_code, "population": c.population , "area_sq_km": c.area_sq_km }
-        import pdb;pdb.set_trace()
         form = CountryForm(data)
         #
-        print("Test valid form A1-1")
         if form.is_valid():
             pass
         else:
             pprint.pprint(form.errors)
         #
-        print("Test valid form A1-2")
-        if False:
-            print(form.as_div())
-            print("Test valid form A2")
-            pprint.pprint(dir(form))
-            print("Test valid form B0")
-            err_dict = form.errors.as_data()
-            print("Test valid form B1")
-            for k, v in err_dict:
-                print(k, " -> ", v)
-            print("Test valid form B2")
-            pprint.pprint(dir(err_dict))
-            print("Test valid form C")
-            #print(form.non_field_errors())
-            print("Test valid form D")
-            print(type(form.is_valid()))
-            print("Test valid form E")
-        #
         self.assertTrue(form.is_valid())
 
-    '''
-    def test_invalid_form(self):
-        c = Country.objects.create(country_iso_code="BBB", population=200, area_sq_km=201)
-        data = { }
-        form = CountryForm(data=data)
+
+    def test_invalid_form_1(self):
+        #
+        data = { "country_iso_code": "AUS", "population": None , "area_sq_km": None }
+        #
+        form = CountryForm(data)
+        #
+        if form.is_valid():
+            pass
+        else:
+            pprint.pprint(form.errors)
+        #
         self.assertFalse(form.is_valid())
-    '''
+
+
+    def test_invalid_form_2(self):
+        #
+        data = { "country_iso_code": "AUS", "population": None , "area_sq_km": 100}
+        #
+        form = CountryForm(data)
+        #
+        self.assertEqual(
+            list(form.errors["population"]), ["This field is required."]
+        )
+
+
+    def test_invalid_form_3(self):
+        #
+        data = { "country_iso_code": "AUS", "population": 100, "area_sq_km": None}
+        #
+        form = CountryForm(data)
+        #
+        self.assertEqual(
+            list(form.errors["area_sq_km"]), ["This field is required."]
+        )
+
